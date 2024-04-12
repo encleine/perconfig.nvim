5f9cc08c31ece08bd93c9eb98a79663f329acdd5
Alabbas Ahmed <alabbas.ahmed@iq-ventures.com>
2024-04-12 15:26:40 +0300

    added nicer fold block visuals

--- lua/custom/Plugins/statuscol.lua
+++ lua/custom/Plugins/statuscol.lua
@@ -1,16 +1,65 @@
+local ffi = require("ffi")
+local C = ffi.C
+
+ffi.cdef([[
+	typedef struct {} win_T;
+	typedef struct {
+		int start;  // line number where deepest fold starts
+		int level;  // fold level, when zero other fields are N/A
+		int llevel; // lowest level that starts in v:lnum
+		int lines;  // number of lines from v:lnum to end of closed fold
+	} foldinfo_T;
+	foldinfo_T fold_info(win_T* wp, int lnum);
+]])
+
+local function foldfunc(args)
+  local width = args.fold.width
+  if width == 0 then return "" end
+
+  local foldinfo = C.fold_info(args.wp, args.lnum)
+  -- I don't know of a better way to do this :\
+  local string = "%#String#"
+  local level = foldinfo.level
+
+  if level == 0 then
+    return string .. (" "):rep(width) .. "%*"
+  end
+
+  local closed = foldinfo.lines > 0
+
+  local range = level < width and level or width
+  if args.virtnum ~= 0 then
+    string = string .. "┃"
+  elseif closed and level == 1 then
+    return string .. "🮰"
+  elseif closed then
+    string = string .. "┇"
+  elseif foldinfo.start == args.lnum and level == 1 then
+    string = string .. "┏"
+  elseif foldinfo.start == args.lnum then
+    string = string .. "┣"
+  else
+    string = string .. "┃"
+  end
+
+  if range < width then
+    string = string .. (" "):rep(width - range)
+  end
+
+  return string .. "%*"
+end
+
 return {
   "luukvbaal/statuscol.nvim",
-  -- this plugin is having some conflicts with neo tree and disaplying the configs seems to fix it :\
+
   config = function()
     local builtin = require("statuscol.builtin")
     require("statuscol").setup(
       {
         relculright = true,
         segments = {
-          {
-            text = { builtin.foldfunc },
-            click = "v:lua.ScFa",
-          },
+          { text = { foldfunc }, },
+
           {
             text = {
               function(args)
@@ -19,12 +68,10 @@ return {
                 return string.rep(' ', 5 - #st) .. st .. ' '
               end,
             },
-            click = "v:lua.ScLa",
           },
           {
             text = { "%s" },
             maxwidth = 1,
-            click = "v:lua.ScSa",
           },
         },
       }
