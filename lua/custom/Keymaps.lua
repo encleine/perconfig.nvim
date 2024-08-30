578545b1c13dd04374a69a12710932c0463d9859
encleine <encleine@gmail.com>
2024-08-30 14:32:28 +0300

    using git shortcuts and added shortcuts for tabs

--- lua/custom/Keymaps.lua
+++ lua/custom/Keymaps.lua
@@ -61,3 +61,10 @@ mmap('{', '}')
 mmap('"', '"')
 mmap("'", "'")
 mmap("`", "`")
+
+[[ making and moving between tabs ]]
+
+vim.keymap.set('n', '<leader>l', ':tabNext <CR>', { silent = true })
+vim.keymap.set('n', '<leader>h', ':tabprevious <CR>', { silent = true })
+vim.keymap.set('n', '<leader>k', ':tabnew <CR>', { silent = true })
+vim.keymap.set('n', '<leader>j', '<C-w>T', { silent = true })
